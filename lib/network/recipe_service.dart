1d421a5c4b6a4c275815a59ebed41b60beff2d9d
reavego <reasantos61@gmail.com>
2023-06-09 21:29:26 +0800

    fasg4

---
+++ lib/network/recipe_service.dart
@@ -0,0 +1,38 @@
+import 'package:chopper/chopper.dart';
+import 'recipe_model.dart';
+import 'model_response.dart';
+import 'model_converter.dart';
+
+part 'recipe_service.chopper.dart';
+
+const String apiKey = '036feb92f0f2a42c4b142daf2a1a2da9';
+const String apiId = 'aca9f0db';
+const String apiUrl = 'https://api.edamam.com';
+
+@ChopperApi()
+abstract class RecipeService extends ChopperService {
+  @Get(path: 'search')
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(
+      @Query('q') String query, @Query('from') int from, @Query('to') int to);
+
+  static RecipeService create() {
+    final client = ChopperClient(
+      baseUrl: apiUrl,
+      interceptors: [_addQuery, HttpLoggingInterceptor()],
+      converter: ModelConverter(),
+      errorConverter: const JsonConverter(),
+      services: [
+        _$RecipeService(),
+      ],
+    );
+    return _$RecipeService(client);
+  }
+}
+
+Request _addQuery(Request req) {
+  final params = Map<String, dynamic>.from(req.parameters);
+  params['app_id'] = apiId;
+  params['app_key'] = apiKey;
+
+  return req.copyWith(parameters: params);
+}
